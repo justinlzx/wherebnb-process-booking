b6b6dacfe4ccab6793f0264c29853666516f9361
justinlzx <justinlee01.jl@gmail.com>
2024-03-13 21:51:59 +0800

    set up

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { routes } from './routes/index.js';
+import chalk from 'chalk';
+import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', routes)
+
+const ENV = process.env
+const NODE_PORT = ENV.NODE_PORT || 3000;
+
+
+app.use(
+  (
+    err,
+    _req,
+    res ,
+    _next ,
+  ) => {
+    console.error(err);
+    const code = typeof err.code === "number" ? err.code : 500;
+   
+  },
+);
+
+
+app.listen(NODE_PORT, async () => {
+  console.log(chalk.bgGreen.white(`APP LISTENING ON PORT ${NODE_PORT}`))
+});
+
+
+
+
+
+
